3c941c258879e74c51ffd9b6cd4233b60a33a382
unknown <hfflora2015@gmail.com>
2022-03-04 20:17:17 -0300

    Aplicativo

---
+++ src/app/services/util.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilService {
+
+  constructor(){
+    
+  }
+}
